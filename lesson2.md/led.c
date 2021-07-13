fc5b30f32178f8faa413f7fbf4c9fb3a56118758
ECoLiI123 <87218693+ECoLiI123@users.noreply.github.com>
2021-07-13 16:51:06 +0800

    Update led.c

--- lesson2.md/led.c
+++ lesson2.md/led.c
@@ -1,1 +1,25 @@
 
+// C++ code
+//
+void setup()
+{
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop()
+{
+  int i=1;
+  for(i=1,i<8;i++)
+  {
+    digitalWrite(i, HIGH);
+    delay(1000); 
+    digitalWrite(i, LOW);
+    delay(1000); 
+  }
+}
